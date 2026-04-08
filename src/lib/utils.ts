a23089ca6eb5aa66106aec74fce178d7321f28cf
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2026-04-08 10:42:04 +0100

    Update vitest configuration
    Test files are now set to run sequentially rather than in parallel to
    ensure idempotency while running tests against the same database.
    
    Errors are no longer logged to the console during tests as they are
    unecessary during testing.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,6 +27,6 @@ export const getRequestUser = (user: Express.User | undefined) => {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.error(err);
+  if (process.env.NODE_ENV !== "test") console.error(err);
   res.status(err.status || 500).json({ message: err.message });
 };
