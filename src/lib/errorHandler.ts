3cfd8a33c929f37d0f1bae9c80b0a49f793dc443
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2026-05-03 14:08:16 +0100

    Initialize project and its dependencies

---
+++ src/lib/errorHandler.ts
@@ -0,0 +1,7 @@
+import type { ErrorRequestHandler } from "express";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err.message);
+  res.status(err.status || 500).json({ message: err.message });
+};
