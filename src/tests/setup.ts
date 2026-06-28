7b9bb63b47dc1f80d664a6d26e24edd6e4b23fad
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2026-06-28 19:37:39 +0100

    Rework the database cleanup function for tests
    The database cleanup function now truncates the database after each test
    as truncates are faster than sequential deletes.

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,20 +1,8 @@
-import { beforeEach } from "vitest";
+import { afterEach } from "vitest";
 import { prisma } from "../lib/prisma.ts";
 
-beforeEach(async () => {
-  const deleteLikes = prisma.like.deleteMany();
-  const deleteBookmarks = prisma.bookmark.deleteMany();
-  const deleteReposts = prisma.repost.deleteMany();
-  const deleteFollows = prisma.follow.deleteMany();
-  const deletePosts = prisma.post.deleteMany();
-  const deleteUsers = prisma.user.deleteMany();
-
-  await prisma.$transaction([
-    deleteLikes,
-    deleteBookmarks,
-    deleteReposts,
-    deleteFollows,
-    deletePosts,
-    deleteUsers,
-  ]);
+afterEach(async () => {
+  await prisma.$executeRawUnsafe(
+    "TRUNCATE TABLE users, posts, bookmarks, follows, likes, reposts CASCADE;",
+  );
 });
