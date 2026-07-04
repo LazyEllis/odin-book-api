71460a43762fce89b1c186c1bea3f57e23802b24
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2026-07-04 21:22:27 +0100

    Log stack traces in the global error handler

--- src/lib/errorHandler.ts
+++ src/lib/errorHandler.ts
@@ -2,6 +2,6 @@ import type { ErrorRequestHandler } from "express";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.error(err.message);
+  console.error(err);
   res.status(err.status || 500).json({ message: err.message });
 };
