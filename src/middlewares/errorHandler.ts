211a4e57e7180386d4b72923c1e66662e9fc8040
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2026-07-25 23:15:44 +0100

    Disable logging during tests

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,6 +2,6 @@ import type { ErrorRequestHandler } from "express";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-  console.error(err);
+  if (process.env.NODE_ENV !== "test") console.error(err);
   res.status(err.status || 500).json({ message: err.message });
 };
